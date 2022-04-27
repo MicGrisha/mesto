37b0d6e8ec7a2930e71a5df29680c3bfc7bf9c95
Grisha Michurov <michurov.grischa@yandex.ru>
2022-04-27 16:05:14 +0200

    На проверку

---
+++ scripts/popup.js
@@ -0,0 +1,44 @@
+const popupOpenBtn = document.querySelector(".profile__edite-button");
+const popup = document.querySelector(".popup");
+const popupCloseBtn = document.querySelector(".popup__close");
+
+popupOpenBtn.addEventListener("click", function (event) {
+  togglePopup(popup);
+});
+
+popupCloseBtn.addEventListener("click", function (event) {
+  togglePopup(popup);
+});
+
+popup.addEventListener("click", function (event) {
+  if (event.target == event.currentTarget) {
+    togglePopup(popup);
+  }
+});
+
+function togglePopup(popupObject) {
+  popupObject.classList.toggle("popup__opened");
+}
+
+
+let formElement = document.querySelector ('.popup__container');
+let nameInput = document.querySelector('#name')
+let jobInput = document.querySelector('#text')
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    document.getElementById('name').value
+    document.getElementById('text').value
+    document.querySelector('.profile__name').textContent = document.getElementById('name').value;
+    document.querySelector('.profile__data').textContent = document.getElementById('text').value;
+}
+formElement.addEventListener('submit', formSubmitHandler); 
+
+
+formElement.addEventListener("click", function (event) {
+  if (event.target == event.currentTarget) {
+    togglePopup(popup);
+  }
+});
+
+
